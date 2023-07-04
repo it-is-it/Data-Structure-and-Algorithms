d97912d830bed2e661cd641848f118c8b74ea485
Ishwor <ishwortimalsina39@gmail.com>
2023-07-04 22:26:45 +0545

    new commit

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Data-Structure-and-Algorithms
 learning dsa from course book
 
+#Day-1:  Learning essential c and c++ concepts
+
+#Day-2: 
--- functions.cpp
+++ functions.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int add(int a, int b)
+{
+    int c;
+    c = a + b;
+    return c;
+};
+
 int main()
 {
-    int a = 10;
+    int num1 = 10, num2 = 15, sum;
+
+    sum = add(num1, num2);
+    cout << "Sum is " << sum;
 
     return 0;
 }
